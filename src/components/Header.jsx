3c092246dc9e2b0af180bbf58cdf659977e14904
Mandeep Singh <mandeep.dev1309@gmail.com>
2024-10-14 19:04:50 +0530

    cool-3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Header.css";
-import rocketsvg from "../assets/rocket.svg";
+import dragon from "../assets/dragon.svg";
 function Header() {
   return (
     <div>
       <div className="main">
-        <img className="img" src={rocketsvg} alt="" />
+        <img className="img" src={dragon} alt="" />
         <h2>Made by Mandeep Singh </h2>
       </div>
     </div>
