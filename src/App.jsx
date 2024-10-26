daddb83619575fefbfd948488093b98370b38162
Mandeep-Singh <mandeep.dev1309@gmail.com>
2024-10-26 19:19:27 +0530

    vercel-speed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import "./App.css";
 
+
+
+import { SpeedInsights } from '@vercel/speed-insights/react';
+
+
+
+
 export default function App() {
   return (
     <div>
       <Header />
       <Body />
       <Footer />
+      <SpeedInsights />
     </div>
   );
 }
