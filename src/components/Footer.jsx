9fd7156b9c704826b00211adbe6753a54996a48e
Mandeep Singh <mandeep.dev1309@gmail.com>
2024-10-30 15:06:47 +0530

    lol

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <p>
-        This project in work in progress.There is many links that don't work
-        due to api's not working together and not filtering .
-        test
-      </p>
+      <p>This test in work in progress.There is many links that don't work.</p>
     </div>
   );
 }
