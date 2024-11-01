1c1367c7e018d304deb2174e17a23e4e3b93608c
Mandeep Singh <mandeep.dev1309@gmail.com>
2024-11-01 18:16:13 +0530

    working but not as imagined

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,34 +3,38 @@ import "./Body.css";
 import SearchIcon from "../assets/search.svg";
 import MovieCard from "./MovieCard";
 
-const Body = ({ searchMovies, movies }) => {
+const Body = ({ searchMovies, movies, loadMore, loadNeed }) => {
   const [searchTerm, setSearchTerm] = useState("");
-  // learn react router. To make home page with vidsrc latest movies catalogs.
+  const [searchDone, setSearchDone] = useState(false);
+
   const handleKeyDown = (e) => {
     if (e.keyCode === 13) {
-      console.log(e.keyCode);
       searchMovies(searchTerm);
+      setSearchDone(true);
+    }
+  };
+
+  const handleSearch = () => {
+    if (searchTerm !== "") {
+      searchMovies(searchTerm);
+      setSearchDone(true);
+    }
+    {
+      console.log("lol-error");
     }
   };
 
   return (
     <div className="app">
-      <h1>movies and series</h1>
+      <h1>Movies and Series</h1>
       <div className="search">
         <input
-          placeholder="Search for a movies or web-series"
+          placeholder="Search for a movie or web series"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           onKeyDown={handleKeyDown}
         />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => {
-            searchMovies(searchTerm);
-            console.log(" searchTerm :", searchTerm);
-          }}
-        />
+        <img src={SearchIcon} alt="search" onClick={handleSearch} />
       </div>
 
       {movies?.length > 0 ? (
@@ -40,9 +44,21 @@ const Body = ({ searchMovies, movies }) => {
           ))}
         </div>
       ) : (
-        <div className="empty">
-          <h2>No movies/series found for given query</h2>
-        </div>
+        searchDone && (
+          <div className="empty">
+            <h2>No movies/series found for the given query</h2>
+          </div>
+        )
+      )}
+
+      {loadNeed && (
+        <button
+          id="myButton"
+          className="load-button"
+          onClick={() => loadMore(searchTerm)}
+        >
+          Load More Content
+        </button>
       )}
     </div>
   );
